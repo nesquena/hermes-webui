Eynzof/hermes-webui-cn

[CN-fork] P-010: 供应商改大模型供应商 + Plugins 面板硬编码 i18n 化

static/i18n.js
@@ -764,6 +764,16 @@ const LOCALES = {
     provider_quota_state_unsupported: 'Unsupported',
     provider_quota_state_no_key: 'No key',
     provider_quota_state_invalid_key: 'Invalid key',
+    settings_section_plugins_title: 'Plugins',
+    settings_section_plugins_meta: 'View installed Hermes plugins and the lifecycle hooks they register. This panel is read-only.',
+    plugins_empty: 'No Hermes plugins are currently visible. Install or enable plugins from the Hermes CLI/config to see them here.',
+    plugins_load_failed: 'Failed to load plugins: ',
+    plugins_no_hooks: 'No registered lifecycle hooks',
+    plugins_no_description: 'No description provided.',
+    plugins_unnamed: 'Unnamed plugin',
+    plugins_enabled: 'Enabled',
+    plugins_disabled: 'Disabled',
+    plugins_registered_hooks: 'Registered hooks',
     cancel: 'Cancel',
     create_job: 'Create job',
     save_skill: 'Save skill',
@@ -6955,28 +6965,45 @@ const LOCALES = {
     suggest_schedule: '今天有什么安排？',
     suggest_files: '这个工作区有哪些文件？',
     sign_out: '退出登录',
-    // Providers panel (English fallback — native translations welcome in follow-up PRs)
-    providers_tab_title: '提供商',
-    providers_section_title: '提供商',
-    providers_section_meta: '管理 AI 提供商的 API 密钥。更改立即生效。',
-    providers_status_configured: 'API 密钥已配置',
-    providers_status_not_configured: '无 API 密钥',
+    // Providers panel
+    providers_tab_title: '大模型供应商',
+    providers_section_title: '大模型供应商',
+    providers_section_meta: '管理大模型供应商的 API key。更改即刻生效。',
+    providers_status_configured: '已配置 API key',
+    providers_status_not_configured: '未配置 API key',
     providers_status_oauth: 'OAuth',
     providers_status_api_key: 'API 密钥',
     providers_status_not_configured_label: '未配置',
-    providers_oauth_hint: '通过 OAuth 认证。无需 API 密钥。',
-    providers_oauth_config_yaml_hint: '通过 config.yaml 配置的令牌。如需更新，请编辑 config.yaml 中的 providers 部分或运行 hermes auth。',
-    providers_oauth_not_configured_hint: '未认证。在终端中运行 hermes auth 以配置此提供商。',
+    providers_oauth_hint: '已通过 OAuth 认证，无需 API key。',
+    providers_oauth_config_yaml_hint: '已通过 config.yaml 配置 token。如需更新，请编辑 config.yaml 中的 providers 段或运行 hermes auth。',
+    providers_oauth_not_configured_hint: '尚未认证。请在终端运行 hermes auth 完成此大模型供应商的配置。',
     providers_save: '保存',
     providers_remove: '移除',
     providers_saving: '保存中…',
     providers_removing: '移除中…',
-    providers_enter_key: '请输入 API 密钥',
-    providers_empty: '未找到可配置的提供商。',
-    providers_key_updated: 'API 密钥已保存',
-    providers_key_removed: 'API 密钥已移除',
+    providers_enter_key: '请输入 API key',
+    providers_empty: '未找到可配置的大模型供应商。',
+    providers_key_updated: 'API key 已保存',
+    providers_key_removed: 'API key 已移除',
     providers_key_placeholder_new: 'sk-...',
-    providers_key_placeholder_replace: '输入新密钥以替换…',
+    providers_key_placeholder_replace: '输入新 key 以替换…',
+    provider_quota_active_title: '当前大模型供应商配额',
+    provider_quota_unavailable_fallback: '配额状态不可用',
+    provider_quota_state_available: '可用',
+    provider_quota_state_unavailable: '不可用',
+    provider_quota_state_unsupported: '不支持',
+    provider_quota_state_no_key: '无 key',
+    provider_quota_state_invalid_key: 'key 无效',
+    settings_section_plugins_title: '插件',
+    settings_section_plugins_meta: '查看已安装的 Hermes 插件及其注册的生命周期钩子。此面板为只读。',
+    plugins_empty: '当前未看到任何 Hermes 插件。可在 Hermes CLI 或 config 中安装/启用插件后查看。',
+    plugins_load_failed: '加载插件失败：',
+    plugins_no_hooks: '未注册任何生命周期钩子',
+    plugins_no_description: '暂无描述。',
+    plugins_unnamed: '未命名插件',
+    plugins_enabled: '已启用',
+    plugins_disabled: '已禁用',
+    plugins_registered_hooks: '已注册的钩子',
     password_placeholder: '输入新密码…',
     password_env_var_locked: '当前已设置 HERMES_WEBUI_PASSWORD 环境变量并具有优先级。请取消该变量并重启服务器，才能在此管理密码。',
     password_env_var_locked_placeholder: '已锁定：已设置 HERMES_WEBUI_PASSWORD 环境变量',
@@ -7338,8 +7365,8 @@ const LOCALES = {
     session_no_selection: '未选择任何会话',
     settings_dropdown_appearance: '外观',
     settings_dropdown_conversation: '对话',
-    settings_dropdown_preferences: '偏好',
-    settings_dropdown_providers: '提供商',
+    settings_dropdown_preferences: '偏好设置',
+    settings_dropdown_providers: '大模型供应商',
     settings_dropdown_system: '系统',
     settings_heading_subtitle: '偏好设置、对话工具和系统控制。',
     settings_heading_title: '控制中心',

nesquena/hermes-webui

static/i18n.js
@@ -7344,12 +7344,12 @@ const LOCALES = {
     settings_busy_input_mode_interrupt: '中断当前轮次',
     settings_busy_input_mode_steer: '引导（中间修正）',
     settings_plugins_title: '插件',
-    settings_plugins_meta: '查看已安装的 Hermes 插件及其注册的生命周期挂钩。此面板为只读。',
-    settings_plugins_empty: '当前没有可见的 Hermes 插件。通过 Hermes CLI/配置文件安装或启用插件后即可在此查看。',
+    settings_plugins_meta: '查看已安装的 Hermes 插件及其注册的生命周期钩子。此面板为只读。',
+    settings_plugins_empty: '当前未看到任何 Hermes 插件。可在 Hermes CLI 或 config 中安装/启用插件后查看。',
     plugins_unnamed: '未命名插件',
     plugins_no_description: '未提供描述信息。',
-    plugins_no_hooks: '未注册生命周期挂钩',
-    plugins_registered_hooks: '已注册的挂钩',
+    plugins_no_hooks: '未注册任何生命周期钩子',
+    plugins_registered_hooks: '已注册的钩子',
     plugins_enabled: '已启用',
     plugins_disabled: '已禁用',
     plugins_load_failed: '加载插件失败：',
@@ -7638,37 +7638,37 @@ const LOCALES = {
     suggest_schedule: '今天有什么安排？',
     suggest_files: '这个工作区有哪些文件？',
     sign_out: '退出登录',
-    // Providers panel (English fallback — native translations welcome in follow-up PRs)
-    providers_tab_title: '提供商',
-    providers_section_title: '提供商',
-    providers_section_meta: '管理 AI 提供商的 API 密钥。更改立即生效。',
-    providers_status_configured: 'API 密钥已配置',
-    providers_status_not_configured: '无 API 密钥',
+    // Providers panel
+    providers_tab_title: '大模型供应商',
+    providers_section_title: '大模型供应商',
+    providers_section_meta: '管理大模型供应商的 API key。更改即刻生效。',
+    providers_status_configured: '已配置 API key',
+    providers_status_not_configured: '未配置 API key',
     providers_status_oauth: 'OAuth',
     providers_status_api_key: 'API 密钥',
     providers_status_not_configured_label: '未配置',
-    providers_oauth_hint: '通过 OAuth 认证。无需 API 密钥。',
-    providers_oauth_config_yaml_hint: '通过 config.yaml 配置的令牌。如需更新，请编辑 config.yaml 中的 providers 部分或运行 hermes auth。',
-    providers_oauth_not_configured_hint: '未认证。在终端中运行 hermes auth 以配置此提供商。',
+    providers_oauth_hint: '已通过 OAuth 认证，无需 API key。',
+    providers_oauth_config_yaml_hint: '已通过 config.yaml 配置 token。如需更新，请编辑 config.yaml 中的 providers 段或运行 hermes auth。',
+    providers_oauth_not_configured_hint: '尚未认证。请在终端运行 hermes auth 完成此大模型供应商的配置。',
     providers_save: '保存',
     providers_remove: '移除',
     providers_saving: '保存中…',
     providers_removing: '移除中…',
-    providers_enter_key: '请输入 API 密钥',
-    providers_empty: '未找到可配置的提供商。',
-    providers_key_updated: 'API 密钥已保存',
-    providers_key_removed: 'API 密钥已移除',
+    providers_enter_key: '请输入 API key',
+    providers_empty: '未找到可配置的大模型供应商。',
+    providers_key_updated: 'API key 已保存',
+    providers_key_removed: 'API key 已移除',
     providers_key_placeholder_new: 'sk-...',
     providers_key_placeholder_replace: '输入新密钥以替换…',
-    provider_quota_title: '当前提供商配额',
-    provider_quota_active_provider: '当前提供商',
+    provider_quota_title: '当前大模型供应商配额',
+    provider_quota_active_provider: '当前大模型供应商',
     provider_quota_last_checked_after_refresh: '刷新后检查',
     provider_quota_last_checked: '上次检查 {0}',
     provider_quota_refresh_usage: '刷新用量',
     provider_quota_refreshing: '正在刷新...',
-    provider_quota_refresh_title: '立即刷新提供商用量限制',
-    provider_quota_refresh_succeeded: '提供商用量已刷新',
-    provider_quota_refresh_failed: '提供商用量刷新失败',
+    provider_quota_refresh_title: '立即刷新大模型供应商用量限制',
+    provider_quota_refresh_succeeded: '大模型供应商用量已刷新',
+    provider_quota_refresh_failed: '大模型供应商用量刷新失败',
     provider_quota_session_limit: '5 小时限制',
     provider_quota_weekly_limit: '每周限制',
     provider_quota_window_fallback: '窗口',
@@ -8087,8 +8087,8 @@ const LOCALES = {
     session_no_selection: '未选择任何会话',
     settings_dropdown_appearance: '外观',
     settings_dropdown_conversation: '对话',
-    settings_dropdown_preferences: '偏好',
-    settings_dropdown_providers: '提供商',
+    settings_dropdown_preferences: '偏好设置',
+    settings_dropdown_providers: '大模型供应商',
     settings_dropdown_system: '系统',
     settings_heading_subtitle: '偏好设置、对话工具和系统控制。',
     settings_heading_title: '控制中心',