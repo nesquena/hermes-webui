868c1b5f347d29dc68c3f883a0f3a1d1933d0733
webtecnica <webtecnica@users.noreply.github.com>
2026-07-19 10:15:19 -0300

    fix: move return after finally to preserve exception propagation

--- static/messages.js
+++ static/messages.js
@@ -1525,8 +1525,9 @@ async function send(){
           else if(typeof expandSidebar==='function') expandSidebar();
           if(typeof renderSessionList==='function') await renderSessionList();
         } finally {
-          $('msg').value='';autoResize();hideCmdDropdown();return;
+          $('msg').value='';autoResize();hideCmdDropdown();
         }
+        return;
       }
       const _agentCmd=typeof getAgentCommandMetadata==='function'
         ? await getAgentCommandMetadata(_parsedCmd.name)
